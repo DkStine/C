360b49c02ae6507998ae5fd3d922d4fa41eb1fd2
DkStine <dkctcak@gmail.com>
2023-06-23 22:38:24 +0530

    name change

--- 01.c
+++ 01.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+int fibonacci(int n) //function definition
+{
+    if (n == 0) //0, 1, 1, 2, 3, 5, 8, 13...
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n-2);
+    }
+
+}
 
 int main()
 {
-    printf("Hellow world");
+    int term, num;
+
+    printf("Note: Terms start from 0\nEnter the required term: ");
+    scanf("%d", &term);
+
+    num = fibonacci(term);
+
+    printf("The term at %d is %d", term, num);
 
     return 0;
 }
