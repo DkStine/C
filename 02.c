bf7612ec999e15ba799a2dc627c60d44c96f3319
DkStine <dkctcak@gmail.com>
2023-06-23 23:01:11 +0530

    commit

---
+++ 02.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int sum(int n)
+{
+    if (n == 0)
+    {
+        printf("Invalid input!");
+    }
+    else if (n == 1)
+    {
+        printf("The sum is %d", 1);
+        return 1;
+    }
+    else
+    {
+        return sum(n) + sum(n-1);
+    }
+}
+
+int main()
+{
+    int n, s;
+
+    printf("Enter no. of terms: ");
+    scanf("%d", &n);
+
+    s = sum(n);
+
+    printf("The sum is %d", s); //1 + 2 + 3 + 4 + 5 = 15
+
+    return 0;
+}
