7aa423f1d0b0cf8fee4b9a8af7f99a1d0a95565d
DkStine <dkctcak@gmail.com>
2023-07-02 23:19:35 +0530

    commit

--- 08/08_02.c
+++ 08/08_02.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    char str_1[10];
-    char *ptr = str_1;
+    // char str_1[10];
+    // char *ptr = str_1;
 
-    printf("This is for using percent c: ");
-    scanf("%c", str_1);
+    // printf("This is for using percent c: ");
+    // scanf("%c", str_1);
 
-    fflush(stdin);
+    // fflush(stdin);
 
-    printf("This is for using percent s: ");
-    scanf("%s", ptr);
+    // printf("This is for using percent s: ");
+    // scanf("%s", ptr);
 
-    printf("%c", ptr);
+    // printf("%c", ptr);
+
+    char *ptr_1 = "far", *ptr_2 = "far";
+
+    printf("%d", strcmp(ptr_1, ptr_2));
 
     return 0;
 }
