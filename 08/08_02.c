0968daad3c0cdc9b5ff9c16705ae75410e55b781
DkStine <dkctcak@gmail.com>
2023-07-02 21:35:56 +0530

    commit

--- 08/08_02.c
+++ 08/08_02.c
@@ -8,8 +8,12 @@ int main()
     printf("This is for using percent c: ");
     scanf("%c", str_1);
 
+    fflush(stdin);
+
     printf("This is for using percent s: ");
     scanf("%s", ptr);
+
+    printf("%c", ptr);
 
     return 0;
 }
