1cd2cf6496a3eddc5ba648f3fcf3923c289ee308
DkStine <dkctcak@gmail.com>
2023-07-30 22:57:03 +0530

    commit

--- 10/10_02.c
+++ 10/10_02.c
@@ -3,7 +3,15 @@
 int main()
 {
     FILE *ptr;
-    
+    ptr = fopen("int.txt", "r");
+
+    int i;
+
+    for (int j = 1; j <= 4; j++)
+    {
+        fscanf(ptr, "%d", &i);
+        printf("%d\n", i);
+    }
 
     return 0;
 }
